844594e358864b4be941efd19579ce5ee9efe95f
Bryan Rizqi Prakosa <bryanrp1950@gmail.com>
2023-10-23 23:43:09 +0700

    Update event_repository.go

--- module/events/repository/mysql/event_repository.go
+++ module/events/repository/mysql/event_repository.go
@@ -47,7 +47,7 @@ func (this *EventReposistory) GetEvent() ([]dto.Event, error) {
 
 	var evts []dto.Event
 
-	err := this.db.Where("status = ?", "publish").Preload("CreatedBy").Preload("Evidences.User").Find(&evts).Error
+	err := this.db.Where("status = ?", "publish").Find(&evts).Preload("CreatedBy").Preload("Evidences.User").Error
 	if err != nil {
 		return []dto.Event{}, err
 	}
