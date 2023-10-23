49a70fa7f223df5ff4780e166f7d75689202ac82
Bryan Rizqi Prakosa <bryanrp1950@gmail.com>
2023-10-23 08:41:31 +0700

    Update user_model.go

--- module/user/repository/model/user_model.go
+++ module/user/repository/model/user_model.go
@@ -8,7 +8,7 @@ import (
 type User struct {
 	*lib.Base
 
-	ID              string
+	ID              string `gorm:"primaryKey"`
 	Name            string
 	Email           string
 	Password        string
